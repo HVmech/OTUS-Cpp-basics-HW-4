6080fddfd2541d0c9b8884dedccc38be47185b57
HVmech <130397692+HVmech@users.noreply.github.com>
2023-10-02 09:59:54 +0300

    Update tokens.hpp

--- include/tokens.hpp
+++ include/tokens.hpp
@@ -5,56 +5,77 @@
 #include "calcexception.hpp"
 #include "astnode.hpp"
 
-class Number : public ASTNode {
-  public:
+class Number : public ASTNode
+{
+public:
     Number(int val)
         : ASTNode(std::to_string(val))
-        {set_val(val);}
+    {
+        set_val(val);
+    }
 };
 
-class Variable : public ASTNode {
-  public:
+class Variable : public ASTNode
+{
+public:
     Variable(std::string name)
         : ASTNode(name)
-        // set value (further improvements)
-        {set_val(1);}
+    // set value (further improvements)
+    {
+        set_val(1);
+    }
 };
 
-class Add : public ASTNode {
-  public:
+class Add : public ASTNode
+{
+public:
     Add(ASTNode *lhs, ASTNode *rhs)
         : ASTNode(std::string(1, '+'), lhs, rhs)
-        {set_val(lhs->value()+rhs->value()); }
+    {
+        set_val(lhs->value()+rhs->value());
+    }
 };
 
-class Sub : public ASTNode {
-  public:
+class Sub : public ASTNode
+{
+public:
     Sub(ASTNode *lhs, ASTNode *rhs)
         : ASTNode(std::string(1, '-'), lhs, rhs)
-        {set_val(lhs->value()-rhs->value()); }
+    {
+        set_val(lhs->value()-rhs->value());
+    }
 };
 
-class Mul : public ASTNode {
-  public:
+class Mul : public ASTNode
+{
+public:
     Mul(ASTNode *lhs, ASTNode *rhs)
         : ASTNode(std::string(1, '*'), lhs, rhs)
-        {set_val(lhs->value()*rhs->value()); }
+    {
+        set_val(lhs->value()*rhs->value());
+    }
 };
 
-class Div : public ASTNode {
-  public:
+class Div : public ASTNode
+{
+public:
     Div(ASTNode *lhs, ASTNode *rhs)
         : ASTNode(std::string(1, '/'), lhs, rhs)
+    {
+        if (rhs->value())
         {
-          if (rhs->value()) {
-            if (rhs->value() == 0){
-              throw calcexception{calcexception::CalcErrorCode::DivisionByZero};
+            if (rhs->value() == 0)
+            {
+                throw calcexception{calcexception::CalcErrorCode::DivisionByZero};
             }
-            else  {
-              set_val(lhs->value()/rhs->value());
+            else
+            {
+                set_val(lhs->value()/rhs->value());
             }
-          } else{
-            throw calcexception{calcexception::CalcErrorCode::DivisionByZero};
-          }
         }
+        else
+        {
+            throw calcexception{calcexception::CalcErrorCode::DivisionByZero};
+        }
+    }
 };
