cdd295589ace08385f6b4b8aef291cd1752c956a
HVmech <130397692+HVmech@users.noreply.github.com>
2023-10-02 09:54:04 +0300

    Update calcexception.hpp

--- include/calcexception.hpp
+++ include/calcexception.hpp
@@ -3,31 +3,33 @@
 #include <iostream>
 #include <system_error>
 
-class calcexception : public std::exception {
-    public:
-        enum class CalcErrorCode {
-            NoError = 0,
-            WrongSyntaxNN,
-            WrongSyntaxNLb,
-            WrongSyntaxOO,
-            WrongSyntaxOE,
-            WrongSyntaxORb,
-            WrongSyntaxLbE,
-            WrongSyntaxLbO,
-            WrongSyntaxLbRb,
-            WrongSyntaxRbN,
-            WrongSyntaxRbLb,
-            WrongSyntaxBraces,
-            WrongSyntaxSpec,
-            DivisionByZero,
-        };
+class calcexception : public std::exception
+{
+public:
+    enum class CalcErrorCode
+    {
+        NoError = 0,
+        WrongSyntaxNN,
+        WrongSyntaxNLb,
+        WrongSyntaxOO,
+        WrongSyntaxOE,
+        WrongSyntaxORb,
+        WrongSyntaxLbE,
+        WrongSyntaxLbO,
+        WrongSyntaxLbRb,
+        WrongSyntaxRbN,
+        WrongSyntaxRbLb,
+        WrongSyntaxBraces,
+        WrongSyntaxSpec,
+        DivisionByZero,
+    };
 
-        explicit calcexception(const CalcErrorCode &errcode)
-            : m_errcode(errcode) 
-        {};
+    explicit calcexception(const CalcErrorCode &errcode)
+        : m_errcode(errcode) 
+    {};
 
-        [[nodiscard]] const char* what() const noexcept override;
+    [[nodiscard]] const char* what() const noexcept override;
 
-    private:
-        const CalcErrorCode m_errcode;
+private:
+    const CalcErrorCode m_errcode;
 };
